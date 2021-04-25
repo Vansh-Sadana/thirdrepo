ed163e624649b4b653028116760fe7b1139f7c4c
unknown <vanshsadana9822@gmail.com>
2021-04-25 11:26:25 +0530

    initial commit thirdrepo

---
+++ mix.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+class mix
+{
+    public static void main(String[] args)
+    {
+        two t = new two();
+        
+    }
+    
+}
+interface one
+{
+    abstract void yes();
+    abstract void gg();
+}
+class two implements one
+{
+        public void yes()
+        {
+            System.out.println("yes vro yes");
+        }
+        public void gg()
+        {
+            System.out.println("yes vro yes");
+        }
+}
